95708bd8c1929b6d7be4228d43c69534b5ca2f82
Rodgersouko <gabrielaouko@gmail.com>
2020-12-17 10:44:33 +0300

    Create User Login endpoint

--- hmsystem/urls.py
+++ hmsystem/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hmsystem.myhmsystem.views import RegisterAPI
+from hmsystem.myhmsystem.views import RegisterAPI, LoginAPI
+from knox import views as knox_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/register/', RegisterAPI.as_view(), name = 'register'),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
